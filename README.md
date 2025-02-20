7a3ba9fd6ad1254125b5f40a31becbacd99f3ec5
rafarha <rafael.rha@gmail.com>
2025-02-20 11:42:02 -0400

    Set up prettier and scaned all files to fix stilization

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 This project is for put some concepts in practice. This is a clone of the website https://www.tabnews.com.br.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function HOME(){
-    return <h1>💖 Oi Priscila, esse é o resultado da minha primeira semana de estudos.🚀</h1>
+function HOME() {
+  return (
+    <h1>
+      💖 Oi Priscila, esse é o resultado da minha primeira semana de estudos.🚀
+    </h1>
+  );
 }
 
 export default HOME;
